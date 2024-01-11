9c61b496227deb397e763f5d6259bd6adcf2e8c1
jpsthecelt <jpsthecelt@gmail.com>
2024-01-11 10:23:16 -0800

    added the comment to indicate that I 'grabbed' my original work via the chrome->figma extension

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import { builder } from '@builder.io/react';
 
 // Notice that this file layout was originally generated 'by hand', then 'tweaked a couple of times'
-//using builder.io and then revamped, again using builder.io import.
+//    using builder.io and then revamped, again using builder.io import.
+//    Additionally, the 'HTML to Figma chrome extension was utilized to capture what I had in the original site.
 
-// Additional features to be added as 'time permits':
+// Following are some additional features to be added as 'time permits':
 //
+// ***** VVV
 // import serialport from 'serialport';
 // import parser-readline from 'serialport';
 // import GPS from 'gps';
@@ -22,6 +24,7 @@ import { builder } from '@builder.io/react';
 // const gps = new GPS();
 
 // const parser = port.pipe(new SerialPortParser());
+// ***** ^^^
 builder.init("d5f47214e7664037a7d7aab11d467bba");
 if ("serial" in navigator) {
     console.log('awesome, we got serial');
